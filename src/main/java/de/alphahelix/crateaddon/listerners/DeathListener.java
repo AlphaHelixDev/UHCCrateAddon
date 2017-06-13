ede0adbed0042ccbc39336da5b9be1c1fefe8e36
AlphaHelixDev <max.hertel@outlook.de>
2017-06-14 00:13:31 +0200

    Added dummie support

--- src/main/java/de/alphahelix/crateaddon/listerners/DeathListener.java
+++ src/main/java/de/alphahelix/crateaddon/listerners/DeathListener.java
@@ -4,15 +4,29 @@ import de.alphahelix.alphalibary.listener.SimpleListener;
 import de.alphahelix.crateaddon.CrateAddon;
 import de.alphahelix.crateaddon.instances.Crate;
 import de.alphahelix.uhcremastered.enums.GState;
+import de.alphahelix.uhcremastered.events.game.DummieDeathEvent;
 import de.alphahelix.uhcremastered.instances.PlayerStatistic;
 import de.alphahelix.uhcremastered.utils.StatsUtil;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
 
 public class DeathListener extends SimpleListener {
 
-    //TODO: PlayerDummieSupport ->  Needs to be done in UHC
+    @EventHandler
+    public void onDeath(DummieDeathEvent e) {
+        if (e.getKiller() != null && e.getKiller() instanceof Player) {
+            Crate c = CrateAddon.getRandomCrate();
+            PlayerStatistic stats = StatsUtil.getStatistics((OfflinePlayer) e.getKiller());
+
+            if (c == null) return;
+            if (Math.random() > c.getRarity()) return;
+
+            stats.addCustomStatistic(c);
+            e.getKiller().sendMessage(CrateAddon.getMessages().getCrateDropped(c.getName()));
+        }
+    }
 
     @EventHandler
     public void onDeath(EntityDeathEvent e) {
